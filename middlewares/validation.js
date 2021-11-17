6479f9a1737ead2b45dfa392789fe9d90689127f
17eugene <drivercoua@gmail.com>
2021-11-17 20:41:04 +0200

    fixed

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -1,9 +1,7 @@
 const validation = (schema) => (req, res, next) => {
   const { error } = schema.validate(req.body);
   if (error) {
-    const favorite = req.body.favorite;
     const postError = new Error(error.message);
-    console.log(error.message);
     postError.status = 400;
     next(postError);
   }
