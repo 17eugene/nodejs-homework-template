3f977738d51530b0fb15c33ae685fd63e837e2b0
17eugene <drivercoua@gmail.com>
2021-11-24 18:19:46 +0200

    cleared the code and added numeric check for page and limit values

--- controllers/contacts-controllers/getAll-ctrl.js
+++ controllers/contacts-controllers/getAll-ctrl.js
@@ -2,38 +2,31 @@ const { contactsModel } = require("../../model/index");
 
 const getAllCtrl = async (req, res, next) => {
   try {
-    const { page = 1, limit = 0, favorite = false } = req.query;
+    let { page = 1, limit = 0, favorite = false } = req.query;
+
+    page = Number(page);
+    limit = Number(limit);
+
+    if (Number.isNaN(page) || Number.isNaN(limit)) {
+      const requestError = new Error("Bad request");
+      requestError.status = 400;
+      throw requestError;
+    }
+
     let skip = (page - 1) * limit;
 
     const { _id } = req.user;
 
-    if (favorite === "true") {
-      const favoriteContacts = await contactsModel
-        .find({ owner: _id, favorite: true }, "_id name email phone owner", {
-          skip,
-          limit: Number(limit),
-        })
-        .populate("owner", "_id email");
-      res.json({
-        status: "success",
-        code: 200,
-        message: "Got favorite",
-        data: {
-          favoriteContacts,
-        },
-      });
-      return;
-    }
-
     const contacts = await contactsModel
-      .find({ owner: _id }, "_id name email phone owner", {
+      .find({ owner: _id, favorite }, "_id name email phone favorite owner", {
         skip,
-        limit: Number(limit),
+        limit: limit,
       })
       .populate("owner", "_id email");
     res.json({
       status: "success",
       code: 200,
+      message: "success",
       data: {
         contacts,
       },
