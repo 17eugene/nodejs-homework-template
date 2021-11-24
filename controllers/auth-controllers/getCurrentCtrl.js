be3e6f1e843ef7da56a98f98a5d08c217694306e
17eugene <drivercoua@gmail.com>
2021-11-24 17:06:49 +0200

    Fix getCurrentCtrl.js - remove DB query

--- controllers/auth-controllers/getCurrentCtrl.js
+++ controllers/auth-controllers/getCurrentCtrl.js
@@ -4,12 +4,11 @@ const getCurrentCtrl = async (req, res, next) => {
   try {
     const { _id, email, subscription } = req.user;
 
-    await usersModel.findById(_id);
-
     res.status(200).json({
       status: "success",
       code: 200,
       data: {
+        _id,
         email,
         subscription,
       },
