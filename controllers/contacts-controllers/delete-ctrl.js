98a197d4366068c20672993c1be5e9c3dd9b2cae
17eugene <drivercoua@gmail.com>
2021-11-25 02:33:45 +0200

    fixed contact deletion by owner

--- controllers/contacts-controllers/delete-ctrl.js
+++ controllers/contacts-controllers/delete-ctrl.js
@@ -3,8 +3,12 @@ const { contactsModel } = require("../../model/index");
 const deleteContactCtrl = async (req, res, next) => {
   try {
     const id = req.params.contactId;
+    const userId = req.user._id;
 
-    const deletedContact = await contactsModel.findByIdAndRemove(id);
+    const deletedContact = await contactsModel.findOneAndRemove({
+      _id: id,
+      owner: userId,
+    });
     if (!deletedContact) {
       const deleteError = new Error("Not found");
       deleteError.status = 404;
