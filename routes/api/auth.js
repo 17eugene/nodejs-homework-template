a5e3c51dadc653e8b5a22b1fbb5f8d733f8f12f5
17eugene <drivercoua@gmail.com>
2021-11-28 14:53:20 +0200

    create /avatars router

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-const { validation } = require("../../middlewares/index");
-const { authenticate } = require("../../middlewares/index");
+const {
+  validation,
+  authenticate,
+  uploadAvatar,
+} = require("../../middlewares/index");
 const { authSchema } = require("../../validation-schemas/index");
 
 const ctrls = require("../../controllers/auth-controllers/index");
+const { avatarUpload } = require("../../middlewares/uploadAvatar");
 
 router.post("/signup", validation(authSchema), ctrls.signupCtrl);
 
@@ -14,5 +18,12 @@ router.post("/signin", validation(authSchema), ctrls.signinCtrl);
 router.get("/signout", authenticate, ctrls.signoutCtrl);
 
 router.get("/current", authenticate, ctrls.getCurrentCtrl);
+
+router.patch(
+  "/avatars",
+  authenticate,
+  uploadAvatar.single("avatarURL"),
+  ctrls.updateAvatarCtrl
+);
 
 module.exports = router;
