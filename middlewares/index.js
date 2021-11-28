b22a7ee0fab98b3416c11cd16f1f9ef4d491471c
17eugene <drivercoua@gmail.com>
2021-11-28 14:56:54 +0200

    re-export uploadAvatar MW

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,7 +1,9 @@
 const { validation } = require("./validation");
 const { authenticate } = require("./authenticate");
+const { uploadAvatar } = require("./uploadAvatar");
 
 module.exports = {
   validation,
   authenticate,
+  uploadAvatar,
 };
